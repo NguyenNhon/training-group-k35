a2156ba76ce5cf1516ebe3b7ba445ee6435fb247
ryenguyen7411 <ryenguyen7411@gmail.com>
2014-08-18 04:36:18 +0000

    ~ Upgrade algorithm :-)

--- RyeNguyen/CompressFile/CompressFile/Decompression.cpp
+++ RyeNguyen/CompressFile/CompressFile/Decompression.cpp
@@ -1,9 +1,6 @@
 #include "Decompression.h"
 
 #include <stdio.h>
-#include <string>
-
-using namespace std;
 
 //Run-Length Decoding algorithm
 void RLE_Decompression ()
@@ -17,62 +14,32 @@ void RLE_Decompression ()
 	fseek (f, 0, SEEK_SET);
 
 	char *buff = new char[size];
-	buff[size] = NULL;
 	fread (buff, size, 1, f);
+	buff[size] = NULL;
 
 	fclose (f);
 	////////////////////////////////////////////////////
 
 	char *p = buff;
-	string str;
+	char *des = new char[size * 1000];
+	int des_length = 0;
 
 	while (p[0] != NULL)
 	{
-		//Control characters for sure
-		if ((p[0] == '\r' && p[1] == '\n') || (p[0] == '\n' && p[-1] == '\r'))
+		for (int i = 0; i < p[0]; i++)
 		{
-			str += p[0];
-		}
-		//Check if there're 2 same character next to each other.
-		//The first of 2 is counts. For example: "AA" mean 65 characters 'A'
-		else if ((p[0] >= 'A' && p[0] <= 'Z') || (p[0] >= 'a' && p[0] <= 'z'))
-		{
-			if (p[1] == p[0])
-			{
-				for (int i = 0; i < p[0]; i++)
-				{
-					str += p[1];
-				}
-
-				p++;
-			}
-			//There's only one character or 2 different characters
-			else
-			{
-				str += p[0];
-			}
-		}
-		//It's counts for sure
-		else
-		{
-			for (int i = 0; i < p[0]; i++)
-			{
-				str += p[1];
-			}
-
-			p++;
+			sprintf (des + des_length, "%c", p[1]);
+			des_length++;
 		}
 
-		p++;
+		p += 2;
 	}
 
 	////////////////////////////////////////////////////
 	//Write from buffer to decode file
-	char *des = (char *)str.c_str ();
-
 	f = fopen ("decode.rye", "wb");
 
-	fwrite (des, 1, str.size (), f);
+	fwrite (des, 1, des_length, f);
 
 	fclose (f);
 	////////////////////////////////////////////////////
